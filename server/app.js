fe0f5a1243e548c790aa95dcec7242616a0403d4
itworld111 <leohaby@hotmail.com>
2018-08-26 22:07:27 +0800

    modified app.js

--- server/app.js
+++ server/app.js
@@ -38,3 +38,41 @@ router.post('/register', function(req, res) {
         }); 
     }); 
 });
+
+router.post('/register-admin', function(req, res) {
+    db.insertAdmin([
+        req.body.name,
+        req.body.email,
+        bcrypt.hashSync(req.body.password, 8),
+        1
+    ],
+    function (err) {
+        if (err) return res.status(500).send("There was a problem registering the user.")
+        db.selectByEmail(req.body.email, (err,user) => {
+            if (err) return res.status(500).send("There was a problem getting user")
+            let token = jwt.sign({ id: user.id }, config.secret, { expiresIn: 86400 // expires in 24 hours
+            });
+            res.status(200).send({ auth: true, token: token, user: user });
+        }); 
+    }); 
+});
+
+router.post('/login', (req, res) => {
+    db.selectByEmail(req.body.email, (err, user) => {
+        if (err) return res.status(500).send('Error on the server.');
+        if (!user) return res.status(404).send('No user found.');
+        let passwordIsValid = bcrypt.compareSync(req.body.password, user.user_pass);
+        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+        let token = jwt.sign({ id: user.id }, config.secret, { expiresIn: 86400 // expires in 24 hours
+        });
+        res.status(200).send({ auth: true, token: token, user: user });
+    });
+})
+
+app.use(router)
+
+let port = process.env.PORT || 3000;
+
+let server = app.listen(port, function() {
+    console.log('Express server listening on port ' + port)
+});
